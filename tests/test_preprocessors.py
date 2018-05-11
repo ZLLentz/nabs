bc3fc28ef459bfbf86c8e64256909ae7230b0676
Zachary Lentz <zlentz@slac.stanford.edu>
2018-05-11 11:37:50 -0700

    TST: Add test for suspend in follow-up, clean up tests a bit

--- tests/test_preprocessors.py
+++ tests/test_preprocessors.py
@@ -10,6 +10,11 @@ from nabs.preprocessors import SuspendPreprocessor
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture(scope='function')
+def sig():
+    return Signal(value=0, name='sig')
+
+
 class NonZeroSuspender(SuspendPreprocessor):
     """
     Suspend on nonzero signal
@@ -33,10 +38,26 @@ def wait_for_future(future):
     logger.debug('done waiting for future')
 
 
+def assert_suspend_release(plan, susp, sig, follow=True):
+    sig.put(1)
+    assert susp._suspend_ev.is_set()
+    assert next(plan).command == 'null'      # pre_plan
+    assert next(plan).command == 'wait_for'  # suspend
+    sig.put(0)
+    wait_for_future(susp._ok_future)
+    assert next(plan).command == 'null'      # post_plan
+    if follow:
+        assert next(plan).command == 'null'  # follow_plan
+
+
+def assert_plan_done(plan):
+    with pytest.raises(StopIteration):
+        next(plan)
+
+
 @pytest.mark.timeout(3)
-def test_suspend_basic():
+def test_suspend_basic(sig):
     logger.debug('test_suspend_basic')
-    sig = Signal(value=0, name='sig')
     susp = NonZeroSuspender(sig, sleep=0.1)
 
     def test_plan():
@@ -49,22 +70,15 @@ def test_suspend_basic():
     # Suspend before the second sleep
     plan = susp(test_plan())
     assert next(plan).command == 'sleep'
-    sig.put(1)
-    assert susp._suspend_ev.is_set()
-    assert next(plan).command == 'null'      # pre_plan
-    assert next(plan).command == 'wait_for'  # suspend
-    sig.put(0)
-    wait_for_future(susp._ok_future)
-    assert next(plan).command == 'null'      # post_plan
-    assert next(plan).command == 'null'      # follow_plan
-    assert next(plan).command == 'sleep'     # back to plan
+    assert_suspend_release(plan, susp, sig)
     assert next(plan).command == 'sleep'
+    assert next(plan).command == 'sleep'
+    assert_plan_done(plan)
 
 
 @pytest.mark.timeout(3)
-def test_suspend_cmd():
+def test_suspend_cmd(sig):
     logger.debug('test_suspend_cmd')
-    sig = Signal(value=0, name='sig')
     susp = NonZeroSuspender(sig, commands=['set'])
 
     def test_plan():
@@ -75,19 +89,32 @@ def test_suspend_cmd():
     cmds = [msg.command for msg in list(susp(test_plan()))]
     assert cmds == ['sleep', 'sleep', 'sleep', 'set']
 
-    # Trip the suspender immediately, make sure we skip suspending until null
+    # Trip the suspender immediately, make sure we skip suspending until set
     plan = susp(test_plan())
     sig.put(1)
     for i in range(3):
         assert next(plan).command == 'sleep'
-    assert susp._suspend_ev.is_set()
-    assert next(plan).command == 'null'      # pre_plan
-    assert next(plan).command == 'wait_for'  # suspend
-    sig.put(0)
-    wait_for_future(susp._ok_future)
-    assert next(plan).command == 'null'      # post_plan
-    assert next(plan).command == 'null'      # follow_plan
-    assert next(plan).command == 'set'       # back to plan
+    assert_suspend_release(plan, susp, sig)
+    assert next(plan).command == 'set'
+    assert_plan_done(plan)
+
+
+@pytest.mark.timeout(3)
+def test_suspend_in_follow_up(sig):
+    logger.debug('test_suspend_in_follow_up')
+    susp = NonZeroSuspender(sig)
+
+    def test_plan():
+        yield from basic_plan(2, 1)
+
+    plan = susp(test_plan())
+    assert next(plan).command == 'sleep'
+    assert_suspend_release(plan, susp, sig, follow=False)
+    # Now we're right before follow-up plan, suspend again
+    assert_suspend_release(plan, susp, sig)
+    assert next(plan).command == 'null'      # follow_plan 2
+    assert next(plan).command == 'sleep'     # back to plan
+    assert_plan_done(plan)
 
 
 def test_needs_override():
